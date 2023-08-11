ed82edea3c5b3171d8fc0bb6b7ad1362b48a3146
oddtO <121516833+oddtO@users.noreply.github.com>
2023-08-11 12:44:00 +0300

    start implementing js

---
+++ js/gameboard.js
@@ -0,0 +1,18 @@
+const WINNING_PATTERNS = [
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+
+[0, 4, 8],
+[2, 4, 6],
+
+[0, 3, 6],
+[1, 4, 7],
+[2, 5, 8]
+
+];
+
+export let gameBoard = +function () {
+	let gridState = [0, 0, 0, 0, 0, 0, 0, 0, 0]         
+
+}();
